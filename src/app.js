f6b79d206bdd04ab0ea3f9b4948027e381696104
danielfercope <danielfercope@gmail.com>
2025-05-14 18:52:46 -0300

    Alteração de path

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors({
-  origin: 'http://localhost:5173' 
+  origin: 'https://save-cambio-frontend.vercel.app/' 
 }));
 
 app.use(express.json());
